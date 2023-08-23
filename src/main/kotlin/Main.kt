a5efa51f33062cee6071da79be4ead474bf7cb2e
diego <20098757@mail.wit.ie>
2023-08-23 02:21:20 +0100

    Menu Added to he Main.kt Controllers, models, persostence,untils packets added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,45 @@
+import utils.ScannerInput
+import utils.ScannerInput.readNextInt
+import utils.ScannerInput.readNextLine
 fun main(args: Array<String>) {
-    println("Hello World!")
+    runMenu()
+}
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun mainMenu() : Int {
+    return ScannerInput.readNextInt(""" 
+         > -------------------------------------
+         > |          NOTE KEEPER APP          |
+         > -------------------------------------
+         > | NOTE MENU                         |
+         > |   1) Add a note                   |
+         > |   2) List notes                   |
+         > |   3) Update a note                |
+         > |   4) Delete a note                |
+         > |   5) Archive a note               |
+         > |   6) Search note(by description)  |
+         > -------------------------------------
+         > |   20) Save notes                  |
+         > |   21) Load notes                  |
+         > -------------------------------------
+         > |   0) Exit                         |
+         > -------------------------------------
+         > ==>> """.trimMargin(">"))
+}
+
+fun runMenu() {
+    do {
+        val option = mainMenu()
+        when (option) {
+            1  -> addNote()
+            2  -> listNotes()
+            3  -> updateNote()
+            4  -> deleteNote()
+            5 -> archiveNote()
+            6 -> searchNotes()
+            20  -> save()
+            21  -> load()
+            0  -> exitApp()
+            else -> println("Invalid option entered: ${option}")
+        }
+    } while (true)
 }
